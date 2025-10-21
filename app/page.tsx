23f15f26f87c7f80319ec9e49560d4a76f744979
Rudra-Pratap-w <ravi03469@gmail.com>
2025-10-21 20:38:23 +0530

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+import { Button } from '@/components/ui/button'
+
+
+const Page = () => {
+  return (
+    <div>
+      <h1 className='text-2xl underline'>
+      <Button>
+        Get Started
+      </Button>
+      </h1>
+    </div>
+  )
+}
+
+export default Page
